3efcd12a08311621e8f08b48ef246e91f412218b
Nagy Gergely <nagygergely.levelek@gmail.com>
2021-08-09 19:25:16 +0200

    kész

---
+++ testproject/mm43.py
@@ -0,0 +1,33 @@
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.support.ui import Select
+import time
+
+# In order for ChromeDriverManager to work you must pip install it in your own environment.
+driver = webdriver.Chrome(ChromeDriverManager().install())
+URL = "https://black-moss-0a0440e03.azurestaticapps.net/mm43.html"
+
+driver.get(URL)
+#visszaadja a hibaüzenetet, de csak akkor ha látszik. Ha nincs hibaüzenet, vagy nem látszik, akkor üressel tér vissza
+def fill_email(email):
+    driver.find_element_by_id("email").clear()
+    driver.find_element_by_id("email").send_keys(email)
+    driver.find_element_by_id("submit").click()
+    hibauzenet=driver.find_elements_by_class_name("validation-error")
+    if (len(hibauzenet)<1):
+        return ""
+    else:
+        if hibauzenet[0].is_enabled():
+            print(hibauzenet[0].text)
+            return hibauzenet[0].text
+        else:
+            return ""
+
+#TC01
+assert "" == fill_email("teszt@elek.hu")
+#TC02
+assert 'Kérjük, adja meg a „@” utáni részt is. A(z) „teszt@” cím nem teljes.' == fill_email("teszt@")
+#TC03
+assert 'Kérjük, töltse ki ezt a mezőt.' == fill_email("")
+
+#
